7f07f8a753814558e718a44213201a7ddfe8f65f
Bekezhan ISSABEK <BISSAB26@neptune.dkcorp.net>
2022-02-16 13:59:55 +0600

    Deleted comments

--- parser/parse.py
+++ parser/parse.py
@@ -2,19 +2,6 @@ from bs4 import BeautifulSoup as bs
 import requests
 import random
 
-# HEADERS = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
-#     'accept': '*/*',
-# }
-# url = 'https://sportmaster.com/ru-kz/product/22116250299/'
-# html =  requests.get(url, headers=HEADERS)
-# soup = bs(html.text, 'html.parser')
-
-# item = soup.find_all('div', class_='cb-item-price-new')
-# print(item)
-# print(item[0].span.text)
-
-
 class Parser:
     def __init__(self, HEADERS):
         self.HEADERS = HEADERS
